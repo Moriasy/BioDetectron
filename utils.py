107900e7fd1c6f1db0c60a7c3f8bf8f5b9da052b
David Bunk <david.bunk@campus.lmu.de>
2020-01-03 12:55:19 +0100

    Added coco2csv conversion function.

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 import os
 import errno
+import numpy as np
+
 from shutil import copytree
 from os.path import isdir, join
 from fnmatch import fnmatch, filter
+
+from pandas import DataFrame
+import pycocotools.coco as coco
+from pycocotools.coco import COCO
 
 
 def include_patterns(*patterns):
@@ -31,3 +37,31 @@ def copy_code(path):
     copytree(py_files_path, path, ignore=include_patterns('*.py', '*.yaml'))
     remove_empty_dirs(path)
 
+
+def coco2csv(dataDir, dataType, annFile, mask=False):
+    coco = COCO(annFile)
+    imgIds = coco.getImgIds(catIds=[])
+
+    for n in imgIds:
+        annIds = coco.getAnnIds(imgIds=[n], catIds=[])
+        anns = coco.loadAnns(annIds)
+
+        img = coco.loadImgs(["{}".format(n)])[0]
+        path = img['file_name']
+
+        for ann in anns:
+            ann["bbox"] = np.asarray(ann["bbox"]).clip(0)
+            ann['bbox'] = np.round(ann["bbox"])
+            ann['x1'] = ann["bbox"][0]
+            ann['y1'] = ann["bbox"][1]
+            ann['x2'] = ann["bbox"][0] + ann["bbox"][2]
+            ann['y2'] = ann["bbox"][1] + ann["bbox"][3]
+
+        df = DataFrame(anns)
+
+        df = df.drop(['area', 'id', 'image_id', 'iscrowd', 'bbox', 'height', 'width'], axis=1)
+
+        if not mask:
+            df = df.drop('segmentation', axis=1)
+
+        df.to_csv(os.path.join(dataDir, dataType, os.path.splitext(path)[0] + '.csv'))
