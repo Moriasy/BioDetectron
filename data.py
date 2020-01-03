4d3b2729c95306278d9113585a3aedd59a749e3a
David Bunk <david.bunk@campus.lmu.de>
2020-01-03 12:38:43 +0100

    Removed own visualization through custom evaluator, as detectron2 got its own tensorboard visualization.

--- data.py
+++ data.py
@@ -80,98 +80,6 @@ def get_csv(root_dir, dataset):
     return dataset_dicts
 
 
-class SKImageLoader(DatasetMapper):
-    def __init__(self, cfg, is_train=True):
-        super().__init__(cfg, is_train=is_train)
-
-    def __call__(self, dataset_dict):
-        """
-        Args:
-            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
-
-        Returns:
-            dict: a format that builtin models in detectron2 accept
-        """
-        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-        # USER: Write your own image loading if it's not from a file
-        image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
-        utils.check_image_size(dataset_dict, image)
-
-        if not self.is_train:
-            dataset_dict['groundtruth'] = copy.deepcopy(dataset_dict)
-            dataset_dict['groundtruth']["image"] = image
-
-        if "annotations" not in dataset_dict:
-            image, transforms = T.apply_transform_gens(
-                ([self.crop_gen] if self.crop_gen else []) + self.tfm_gens, image
-            )
-        else:
-            # Crop around an instance if there are instances in the image.
-            # USER: Remove if you don't use cropping
-            if self.crop_gen:
-                crop_tfm = utils.gen_crop_transform_with_instance(
-                    self.crop_gen.get_crop_size(image.shape[:2]),
-                    image.shape[:2],
-                    np.random.choice(dataset_dict["annotations"]),
-                )
-                image = crop_tfm.apply_image(image)
-            image, transforms = T.apply_transform_gens(self.tfm_gens, image)
-            if self.crop_gen:
-                transforms = crop_tfm + transforms
-
-        image_shape = image.shape[:2]  # h, w
-
-        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
-        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
-        # Therefore it's important to use torch.Tensor.
-
-        dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
-
-        # Can use uint8 if it turns out to be slow some day
-
-        # USER: Remove if you don't use pre-computed proposals.
-        if self.load_proposals:
-            utils.transform_proposals(
-                dataset_dict, image_shape, transforms, self.min_box_side_len, self.proposal_topk
-            )
-
-        if "annotations" in dataset_dict:
-            # USER: Modify this if you want to keep them for some reason.
-            for anno in dataset_dict["annotations"]:
-                if not self.mask_on:
-                    anno.pop("segmentation", None)
-                if not self.keypoint_on:
-                    anno.pop("keypoints", None)
-
-            # USER: Implement additional transformations if you have other types of data
-            annos = [
-                utils.transform_instance_annotations(
-                    obj, transforms, image_shape, keypoint_hflip_indices=self.keypoint_hflip_indices
-                )
-                for obj in dataset_dict.pop("annotations")
-                if obj.get("iscrowd", 0) == 0
-            ]
-
-            instances = utils.annotations_to_instances(
-                annos, image_shape, mask_format=self.mask_format
-            )
-            # Create a tight bounding box from masks, useful when image is cropped
-            if self.crop_gen and instances.has("gt_masks"):
-                instances.gt_boxes = instances.gt_masks.get_bounding_boxes()
-
-            dataset_dict["instances"] = utils.filter_empty_instances(instances)
-
-        # USER: Remove if you don't do semantic/panoptic segmentation.
-        if "sem_seg_file_name" in dataset_dict:
-            with PathManager.open(dataset_dict.pop("sem_seg_file_name"), "rb") as f:
-                sem_seg_gt = Image.open(f)
-                sem_seg_gt = np.asarray(sem_seg_gt, dtype="uint8")
-            sem_seg_gt = transforms.apply_segmentation(sem_seg_gt)
-            sem_seg_gt = torch.as_tensor(sem_seg_gt.astype("long"))
-            dataset_dict["sem_seg"] = sem_seg_gt
-        return dataset_dict
-
-
 class BoxDetectionLoader(DatasetMapper):
     def __init__(self, cfg, is_train=True):
         super().__init__(cfg, is_train=is_train)
@@ -201,11 +109,6 @@ class BoxDetectionLoader(DatasetMapper):
 
         ### CUSTOM NORMALIZATION ?
         ### CHECK DATATYPE HERE !
-
-        # If loading images for evaluation, save original data before augmentation for visualisation.
-        if not self.is_train:
-            dataset_dict['groundtruth'] = copy.deepcopy(dataset_dict)
-            dataset_dict['groundtruth']["image"] = image
 
         # Convert bounding boxes to imgaug format for augmentation.
         boxes = []
--- eval.py
+++ eval.py
@@ -14,8 +14,6 @@ from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer
 from detectron2.evaluation import DatasetEvaluator
 
-from data import SKImageLoader
-
 
 class GenericEvaluator(DatasetEvaluator):
     def __init__(self, dataset_name, cfg, output_dir, distributed=False):
@@ -26,8 +24,6 @@ class GenericEvaluator(DatasetEvaluator):
 
         self._cpu_device = torch.device("cpu")
         self._logger = logging.getLogger(__name__)
-
-        self.img_loader = SKImageLoader(cfg, is_train=False)
 
         self._metadata = MetadataCatalog.get(dataset_name)
 
@@ -49,7 +45,7 @@ class GenericEvaluator(DatasetEvaluator):
 
     def process(self, inputs, outputs):
         for input, output in zip(inputs, outputs):
-            prediction = {'groundtruth':input["groundtruth"]}
+            prediction = {'groundtruth':input}
 
             # TODO this is ugly
             if "instances" in output:
@@ -88,134 +84,8 @@ class GenericEvaluator(DatasetEvaluator):
     def _eval_predictions(self, task):
         for n in range(len(self._predictions)):
             metadata = MetadataCatalog.get(self._dataset_name)
-
             image = self._predictions[n]["groundtruth"]["image"]
 
-            viz = Visualizer(image, metadata)
-            viz.draw_dataset_dict(self._predictions[n]["groundtruth"]).save(
-                os.path.join(self._output_dir, 'GT_{}.png'.format(n)))
-
-            viz = Visualizer(image, metadata)
-            viz.draw_instance_predictions(self._predictions[n]["instances"]).save(
-                os.path.join(self._output_dir, 'pred_{}.png'.format(n)))
+        # ADD EVALUATION SCORES!
 
         return
-
-
-class VizdomVisualizer:
-    """
-    Based on the vizdom tools from Pix2Pix:
-    https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
-    """
-    def __init__(self):
-        self.ncols = 4
-        self.vis = visdom.Visdom(server="http://localhost", port=1337, env="main")
-        self.port = 1337
-
-        if not self.vis.check_connection():
-            self.create_visdom_connections()
-
-    def create_visdom_connections(self):
-        """If the program could not connect to Visdom server, this function will start a new server at port < self.port > """
-        cmd = sys.executable + ' -m visdom.server -p %d &>/dev/null &' % self.port
-        print('\n\nCould not connect to Visdom server. \n Trying to start a server....')
-        print('Command: %s' % cmd)
-        Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-
-    def display_current_results(self, visuals):
-        """Display current results on visdom; save current results to an HTML file.
-        Parameters:
-            visuals (OrderedDict) - - dictionary of images to display or save
-            epoch (int) - - the current epoch
-            save_result (bool) - - if save the current results to an HTML file
-        """
-        ncols = self.ncols
-        if ncols > 0:        # show all the images in one visdom panel
-            ncols = min(ncols, len(visuals))
-            h, w = next(iter(visuals.values())).shape[:2]
-            table_css = """<style>
-                    table {border-collapse: separate; border-spacing: 4px; white-space: nowrap; text-align: center}
-                    table td {width: % dpx; height: % dpx; padding: 4px; outline: 4px solid black}
-                    </style>""" % (w, h)  # create a table css
-            # create a table of images.
-            title = self.name
-            label_html = ''
-            label_html_row = ''
-            images = []
-            idx = 0
-            for label, image in visuals.items():
-                image_numpy = util.tensor2im(image)
-                label_html_row += '<td>%s</td>' % label
-                images.append(image_numpy.transpose([2, 0, 1]))
-                idx += 1
-                if idx % ncols == 0:
-                    label_html += '<tr>%s</tr>' % label_html_row
-                    label_html_row = ''
-            white_image = np.ones_like(image_numpy.transpose([2, 0, 1])) * 255
-            while idx % ncols != 0:
-                images.append(white_image)
-                label_html_row += '<td></td>'
-                idx += 1
-            if label_html_row != '':
-                label_html += '<tr>%s</tr>' % label_html_row
-            try:
-                self.vis.images(images, nrow=ncols, win=1,
-                                padding=2, opts=dict(title=title + ' images'))
-                label_html = '<table>%s</table>' % label_html
-                self.vis.text(table_css + label_html, win=2,
-                              opts=dict(title=title + ' labels'))
-            except VisdomExceptionBase:
-                self.create_visdom_connections()
-
-        else:     # show each image in a separate visdom panel;
-            idx = 1
-            try:
-                for label, image in visuals.items():
-                    image_numpy = util.tensor2im(image)
-                    self.vis.image(image_numpy.transpose([2, 0, 1]), opts=dict(title=label),
-                                   win=self.display_id + idx)
-                    idx += 1
-            except VisdomExceptionBase:
-                self.create_visdom_connections()
-
-    def plot_current_losses(self, epoch, counter_ratio, losses):
-        """display the current losses on visdom display: dictionary of error labels and values
-        Parameters:
-            epoch (int)           -- current epoch
-            counter_ratio (float) -- progress (percentage) in the current epoch, between 0 to 1
-            losses (OrderedDict)  -- training losses stored in the format of (name, float) pairs
-        """
-        if not hasattr(self, 'plot_data'):
-            self.plot_data = {'X': [], 'Y': [], 'legend': list(losses.keys())}
-        self.plot_data['X'].append(epoch + counter_ratio)
-        self.plot_data['Y'].append([losses[k] for k in self.plot_data['legend']])
-        try:
-            self.vis.line(
-                X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
-                Y=np.array(self.plot_data['Y']),
-                opts={
-                    'title': self.name + ' loss over time',
-                    'legend': self.plot_data['legend'],
-                    'xlabel': 'epoch',
-                    'ylabel': 'loss'},
-                win=self.display_id)
-        except VisdomExceptionBase:
-            self.create_visdom_connections()
-
-    # losses: same format as |losses| of plot_current_losses
-    def print_current_losses(self, epoch, iters, losses, t_comp, t_data):
-        """print current losses on console; also save the losses to the disk
-        Parameters:
-            epoch (int) -- current epoch
-            iters (int) -- current training iteration during this epoch (reset to 0 at the end of every epoch)
-            losses (OrderedDict) -- training losses stored in the format of (name, float) pairs
-            t_comp (float) -- computational time per data point (normalized by batch_size)
-            t_data (float) -- data loading time per data point (normalized by batch_size)
-        """
-        message = '(epoch: %d, iters: %d, time: %.3f, data: %.3f) ' % (epoch, iters, t_comp, t_data)
-        for k, v in losses.items():
-            message += '%s: %.3f ' % (k, v)
-
-        print(message)  # print the message
-        with open(self.log_name, "a") as log_file:
-            log_file.write('%s\n' % message)  # save the message
