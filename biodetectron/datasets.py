3a53a95b9a713b2bac3f21d7426bbb4375538b03
David Bunk <bunk@biologie.uni-muenchen.de>
2021-07-29 12:47:06 +0200

    Changed augmentation

--- biodetectron/datasets.py
+++ biodetectron/datasets.py
@@ -2,19 +2,19 @@ from imgaug import augmenters as iaa
 from detectron2.data import DatasetCatalog, MetadataCatalog
 
 
-def get_custom_augmenters(name, max_size, is_train, image_shape):  
+def get_custom_augmenters(name, max_size, is_train):  
     seq = iaa.Sequential([])
 
     if "yeastmate" in name:
         if is_train:
             seq = iaa.Sequential([
-                iaa.CropToFixedSize(width=max_size, height=max_size),
+                iaa.CropToFixedSize(width=400, height=400),
                 iaa.Fliplr(0.5),
                 iaa.Flipud(0.5),
                 iaa.Rot90(k=(0, 3)),
-                iaa.Sometimes(0.33, iaa.GammaContrast(gamma=(0.8, 1.2))),
-                iaa.Sometimes(0.5, iaa.Multiply(mul=(0.5, 1.5))),
-                iaa.Sometimes(0.1, iaa.GaussianBlur(sigma=1.0))
+                # iaa.Sometimes(0.33, iaa.GammaContrast(gamma=(0.8, 1.2))),
+                # iaa.Sometimes(0.5, iaa.Multiply(mul=(0.5, 1.5))),
+                # iaa.Sometimes(0.1, iaa.GaussianBlur(sigma=1.0))
             ])
 
         else:
@@ -57,8 +57,8 @@ def register_custom_datasets():
     path_dict = {}
 
     ####### YEASTMATE DATA
-    dict_getter = DictGetter("yeastmate", train_path='/scratch/bunk/osman/budding/paper/train',
-                             val_path='/scratch/bunk/osman/budding/paper/val', mask=True)
+    dict_getter = DictGetter("yeastmate", train_path='/scratch/bunk/osman/budding/paper_all_mid/train',
+                             val_path='/scratch/bunk/osman/budding/paper_all_mid/val', mask=True)
 
     path_dict["yeastmate"] = dict_getter.train_path
 
